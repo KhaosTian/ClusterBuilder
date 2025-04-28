61bc49e9cdeb5aee41706d5175fd6de10e819dff
Khaos Tian <just.tzh@outlook.com>
2025-04-28 18:27:22 +0800

    Update DisjointSet.h

--- Source/DisjointSet.h
+++ Source/DisjointSet.h
@@ -2,6 +2,8 @@
 
 #include <vector>
 
+#include "Common.h"
+
 class DisjointSet
 {
 public:
@@ -21,3 +23,60 @@ public:
 private:
     std::vector<uint32_t> m_Parents;
 };
+
+FORCEINLINE DisjointSet::DisjointSet(uint32_t size) { Init(size); }
+
+FORCEINLINE void DisjointSet::Init(uint32_t size)
+{
+    m_Parents.reserve(size);
+    for (uint32_t i = 0; i < size; i++)
+    {
+        m_Parents[i] = i;
+    }
+}
+
+FORCEINLINE void DisjointSet::Reset() { m_Parents.clear(); }
+
+FORCEINLINE void DisjointSet::AddDefaulted(uint32_t num)
+{
+    uint32_t start = m_Parents.size();
+    m_Parents.reserve(start + num);
+    for (uint32_t i = start; i < start + num; i++)
+    {
+        m_Parents[i] = i;
+    }
+}
+
+FORCEINLINE void DisjointSet::Union(uint32_t x, uint32_t y)
+{
+    uint32_t px = m_Parents[x];
+    uint32_t py = m_Parents[y];
+
+    while (px != py)
+    {
+        if (px < py)
+        {
+            m_Parents[x] = py;
+            if (x == px)
+            {
+                return;
+            }
+            x = px;
+            px = m_Parents[x];
+        }
+        else {
+            m_Parents[y] = px;
+            if (y == py)
+            {
+                return;
+            }
+            y = py;
+            py = m_Parents[y];
+        }
+    }
+
+}
+
+FORCEINLINE void DisjointSet::UnionSequential(uint32_t x, uint32_t y) {}
+
+FORCEINLINE uint32_t DisjointSet::Find(uint32_t i) { return 0; }
