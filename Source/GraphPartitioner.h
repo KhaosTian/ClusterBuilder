ebfdddff56042ed6f05843982537e33c520d3dc9
Khaos Tian <just.tzh@outlook.com>
2025-04-30 15:38:16 +0800

    更新

--- Source/GraphPartitioner.h
+++ Source/GraphPartitioner.h
@@ -20,7 +20,7 @@ public:
         bool operator<(const Range& other) const { return begin < other.begin; }
     };
 
-    std::vector<Range>    ranges;
+    std::vector<Range>  ranges;
     std::vector<uint32> indexes;
     std::vector<uint32> sorted_to;
 
@@ -31,8 +31,13 @@ public:
     void AddAdjaceny(GraphData* graph, uint32 adj_index, idx_t cost);
     void AddLocalityLinks(GraphData* graph, uint32 index, idx_t cost);
 
-    template <typename FuncType>
-    void BuildLocalityLinks(DisjointSet& disjoint_set, const Bounds3f& bounds, const std::vector<int32>& group_indexes, FuncType& GetCenter);
+    template<typename FuncType>
+    void BuildLocalityLinks(
+        DisjointSet&              disjoint_set,
+        const Bounds3f&           bounds,
+        const std::vector<int32>& group_indexes,
+        FuncType&                 GetCenter
+    );
 
     void Partition(GraphData* graph);
 
@@ -41,9 +46,8 @@ public:
     void BisectGraph(GraphData* graph, GraphData* child_graphs[2]);
     void RecursiveBisectGraph(GraphData* graph);
 
-
     uint32 m_num_elements;
-    int32 m_min_partition_size;
+    int32  m_min_partition_size;
     int32  m_max_partition_size;
 
     std::atomic<uint32> m_num_parition;
@@ -55,5 +59,22 @@ public:
 };
 
 template<typename FuncType>
-FORCEINLINE void GraphPartitioner::BuildLocalityLinks(DisjointSet& disjoint_set, const Bounds3f& bounds, const std::vector<int32>& group_indexes, FuncType& GetCenter) {
+FORCEINLINE void GraphPartitioner::BuildLocalityLinks(
+    DisjointSet&              disjoint_set,
+    const Bounds3f&           bounds,
+    const std::vector<int32>& group_indexes,
+    FuncType&                 GetCenter
+) {
+    std::vector<uint32> sort_keys;
+    sort_keys.reserve(m_num_elements);
+    sorted_to.reserve(m_num_elements);
+
+    const bool enable_element_groups = !group_indexes.empty();
+
+    ParallelFor("BuildLocalityLinks.ParallelFor", m_num_elements, 4096, [&](uint32 index) {
+        Vector3f center       = GetCenter(index);
+        Vector3f cenetr_local = (center - bounds.minP) / Vector3f(bounds.maxP - bounds.minP).max;
+
+        uint32 morton;
+    });
 }
--- Source/main.cpp
+++ Source/main.cpp
@@ -12,7 +12,13 @@ struct Vert {
     std::vector<Vector3f> Positions;
 };
 
-static void ClusterTriangles(Vert& verts, const std::vector<uint32>& indexes, const std::vector<int32>& material_indexes, std::vector<Cluster>& clusters, const Bounds3f& mesh_bounds) {
+static void ClusterTriangles(
+    Vert&                      verts,
+    const std::vector<uint32>& indexes,
+    const std::vector<int32>&  material_indexes,
+    std::vector<Cluster>&      clusters,
+    const Bounds3f&            mesh_bounds
+) {
     uint32 num_triangles = static_cast<uint32>(indexes.size() / 3);
 
     Adjacency adjacency { indexes.size() };
@@ -21,7 +27,9 @@ static void ClusterTriangles(Vert& verts, const std::vector<uint32>& indexes, co
     auto GetPosition = [&verts, &indexes](uint32 edge_index) { return verts.Positions[indexes[edge_index]]; };
 
     // 将每个索引视作一条边，构建边的哈希表
-    ParallelFor("ClusterTriangles.ParalleFor", indexes.size(), 4096, [&](int edge_index) { edge_hash.AddConcurrent(edge_index, GetPosition); });
+    ParallelFor("ClusterTriangles.ParalleFor", indexes.size(), 4096, [&](int edge_index) {
+        edge_hash.AddConcurrent(edge_index, GetPosition);
+    });
 
     // 将每个索引视作一条边，确定边的邻接关系
     ParallelFor("ClusterTriangles.ParalleFor", indexes.size(), 1024, [&](int edge_index) {
@@ -48,7 +56,9 @@ static void ClusterTriangles(Vert& verts, const std::vector<uint32>& indexes, co
         if (adjacency.direct[edge_index] == -2) {
             std::vector<std::pair<int32, int32>> edges;
             // 收集所有匹配当前边的边
-            edge_hash.ForAllMatching(edge_index, false, GetPosition, [&](int32 edge_index0, int32 edge_index1) { edges.emplace_back(edge_index0, edge_index1); });
+            edge_hash.ForAllMatching(edge_index, false, GetPosition, [&](int32 edge_index0, int32 edge_index1) {
+                edges.emplace_back(edge_index0, edge_index1);
+            });
 
             // 标准库排序保证确定性
             std::sort(edges.begin(), edges.end());
@@ -70,21 +80,50 @@ static void ClusterTriangles(Vert& verts, const std::vector<uint32>& indexes, co
     }
 
     // 初始化图划分器
-    GraphPartitioner patitioner(num_triangles, Cluster::ClusterSize - 4, Cluster::ClusterSize);
+    GraphPartitioner partitioner(num_triangles, Cluster::ClusterSize - 4, Cluster::ClusterSize);
     {
         // 获取三角形的中心坐标
-        auto GetCenter = [&verts, &indexes](uint32 tri_index) { Vector3f center;
+        auto GetCenter = [&verts, &indexes](uint32 tri_index) {
+            Vector3f center;
             center = verts.Positions[indexes[tri_index * 3 + 0]];
             center += verts.Positions[indexes[tri_index * 3 + 1]];
             center += verts.Positions[indexes[tri_index * 3 + 2]];
             return center * (1.0f / 3.0f);
         };
 
-        patitioner.BuildLocalityLinks(disjoint_set, mesh_bounds, material_indexes, GetCenter);
+        // 建立邻接关系
+        partitioner.BuildLocalityLinks(disjoint_set, mesh_bounds, material_indexes, GetCenter);
 
         // restrict 保证只有这个指针指向这块内存，方便编译器优化，若违反则可能导致未定义行为
-        auto* RESTRICT graph = patitioner.NewGraph(num_triangles * 3);
+        auto* RESTRICT graph = partitioner.NewGraph(num_triangles * 3);
+
+        // 遍历每个三角形
+        for (uint32 i = 0; i < num_triangles; i++) {
+            graph->adjacency_offset[i] = graph->adjacency.size();// 设置邻接表偏移量
+            uint32 tri_index           = partitioner.indexes[i];// 获取三角形索引
+            // 遍历三角形的三个边
+            for (int k = 0; k < 3; k++) {
+                // 遍历边的所有邻接边
+                adjacency.ForAll(tri_index * 3 + k, [&partitioner, graph](int32 edge_index, int32 adj_index) {
+                    partitioner.AddAdjaceny(graph, adj_index / 3, idx_t(4 * 65)); // 将邻接边所在的三角形索引添加到邻接三角形
+                });
+            }
+
+            // 将该三角形索引添加
+            partitioner.AddLocalityLinks(graph, tri_index, 1);
+        }
+
+        // 设置最后一个三角形的邻接偏移量
+        graph->adjacency_offset[num_triangles] = graph->adjacency.size();
+
+        // 三角形超过5000个则启用多线程划分
+        bool enable_multi_threaded = num_triangles >= 5000;
+        partitioner.ParititionStrict(graph, enable_multi_threaded);
+
+        CHECK(partitioner.ranges.size());
     }
+
+
 }
 
 int main() {
